849bc0fb3434835f498acba9dcc62b46e8a17898
Alibek Olimov <98762357+OlimovAlibek@users.noreply.github.com>
2024-09-14 15:24:10 +0500

    Update testbc.js

--- testbc.js
+++ testbc.js
@@ -12,3 +12,283 @@ function birthday(s, d, m) {
 }
 
 birthday([2,2,1,3,2],4,2)
+
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
+//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)//
+function birthday(s, d, m) {
+    let array = []
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] + s[i + 1] == d) {
+        array.push([s[i], s[i+1]])
+      }
+    }
+  
+  return array.length
+
+}
+
+birthday([2,2,1,3,2],4,2)
