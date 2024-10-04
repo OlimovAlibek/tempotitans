8a5f09a657783733969d530088c73d9f891d66ad
Alibek Olimov <98762357+OlimovAlibek@users.noreply.github.com>
2024-10-04 15:56:26 +0500

    Update jstest.js

--- jstest.js
+++ jstest.js
@@ -1,3 +1,18 @@
-//
-//
-//
+function moveTen(s){
+  let letters = s.split("")
+  let numbers = []
+  
+  for(let i = 0; i < letters.length; i++) {
+    numbers[i] = letters[i].charCodeAt() + 10
+    
+    if (numbers[i] > 122) {
+      numbers[i] = 96 + (numbers[i] - 122)
+    }
+    
+  }
+  return (numbers.map((item) => String.fromCharCode(item))).join("")
+}
+
+
+
+moveTen("alz")
